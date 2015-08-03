1fc576e6fe529874f8a9edc83231e55ae2f97838
Aniruddha Varma R <ShaperCouncil@gmail.com>
2015-08-03 10:34:24 +0530

    Putting into Git
    This is, as of now, proof of concept - a WPF translation of the Zone
    Simulator that was made in GM8. This project simulates some of the floor
    (around upto 30 floor numbers). The graphic locations are hard coded,
    they must be in drive a:\ for this project to compile out of the box.

---
+++ G5 Delicious Drayk 3D/Floor.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * The floorsets (and possibly wallsets) are replaceable and are numbered the same in the
+ * ScenData file, ie a floorset points to one of several inter-changeable sprite sheets
+ * such as 'Snow' and 'Grass'
+ * 
+ */
+namespace DMCGeneforgeMpTiles
+{
+    class cFloor
+    {
+        public static cFloor[] Floors;
+
+        //default values to save definition code (changes to reflect most recent definition)
+        public static int _iGraphicTemplate;    // fl_graphic_template
+        public static int _iRow;                // fl_graphic_sheet  (begins 0)
+        public static int _iCol;                // fl_which_icon     (begins 0)
+
+        
+        int iGraphicTemplate;
+
+        public void setGraphicTemplate(int i)
+        {
+            iGraphicTemplate = _iGraphicTemplate = i;
+        }
+
+        int iRow;
+
+        public void setRow(int i)
+        {
+            iRow = _iRow = i;
+        }
+        
+        int iCol;
+
+        public void setCol(int i)
+        {
+            iCol = _iCol = i;
+        }
+
+        cFloor()
+        {
+            //Init with the default(i.e. static) values. If values are different than this, can be changed later
+            iGraphicTemplate = _iGraphicTemplate;
+            iRow = _iRow;
+            iCol = _iCol;
+        }
+        cFloor(ref cFloor old)
+        {
+            iGraphicTemplate = _iGraphicTemplate = old.iGraphicTemplate;
+            iRow = _iRow = old.iRow;
+            iCol = _iCol = old.iCol;
+        }
+
+        public static bool RetrieveRowAndCol(int iFloorNum,ref int iRow,ref int iCol)
+        {
+            if (iFloorNum > 28) return false;
+            iRow = Floors[iFloorNum].iRow;
+            iCol = Floors[iFloorNum].iCol;
+            return true;
+        }
+
+        /// <summary>
+        /// Define the array of all floor types
+        /// </summary>
+        public static void InitializeFloors()
+        {
+            //define the default values to be reused
+            cFloor._iGraphicTemplate = 0;
+            cFloor._iRow = 0;
+            cFloor._iCol = 0;
+
+            Floors = new cFloor[100];
+            //6 basic grounds
+            Floors[0] = new cFloor(); //Ground
+            Floors[1] = new cFloor(); Floors[1].setCol(1);
+            Floors[2] = new cFloor(); Floors[2].setCol(2);
+            Floors[3] = new cFloor(); Floors[3].setCol(3);
+            Floors[4] = new cFloor(); Floors[4].setCol(4);
+            Floors[5] = new cFloor(); Floors[5].setCol(5);
+            //Grass/Water
+            Floors[6] = new cFloor(); Floors[6].setCol(6); //Water
+            Floors[7] = new cFloor(); Floors[7].setCol(7); //DeepWater
+            Floors[8] = new cFloor(); Floors[8].setCol(0); //Shore
+             Floors[8].setRow(1);
+            Floors[9] = new cFloor(); Floors[9].setCol(1);
+            Floors[10] = new cFloor(); Floors[10].setCol(2);
+            Floors[11] = new cFloor(); Floors[11].setCol(3);
+            Floors[12] = new cFloor(); Floors[12].setCol(4);
+            Floors[13] = new cFloor(); Floors[13].setCol(5);
+            Floors[14] = new cFloor(); Floors[14].setCol(6);
+            Floors[15] = new cFloor(); Floors[15].setCol(0);
+             Floors[15].setRow(2);
+            Floors[16] = new cFloor(); Floors[16].setCol(1);
+            Floors[17] = new cFloor(); Floors[17].setCol(2);
+            Floors[18] = new cFloor(); Floors[18].setCol(3);
+            Floors[19] = new cFloor(); Floors[19].setCol(4);
+            Floors[20] = new cFloor(ref Floors[7]); // Water w Island
+             Floors[20].setRow(2);
+             Floors[20].setCol(5);
+            Floors[21] = new cFloor(); Floors[21].setCol(6); // Water w Rock
+            //grass with misc features
+            Floors[25] = new cFloor(ref Floors[0]); //Boulder
+             Floors[25].setRow(3);
+             Floors[25].setCol(0);
+            Floors[26] = new cFloor(); Floors[26].setCol(1);
+            Floors[27] = new cFloor(); Floors[27].setCol(2); //Huge boulder blocks whole space
+            Floors[28] = new cFloor(ref Floors[0]);//Stone Path
+             Floors[28].setRow(3);
+             Floors[28].setCol(3);
+            Floors[29] = new cFloor(ref Floors[0]); // Crops
+             Floors[29].setCol(4);
+             Floors[29].setRow(3);
+            Floors[30] = new cFloor(ref Floors[27]); // Pool
+             Floors[30].setRow(3);
+             Floors[30].setCol(5);
+            Floors[31] = new cFloor(ref Floors[0]); // Rocky Ground
+             Floors[31].setRow(3);
+             Floors[31].setCol(6);
+            Floors[32] = new cFloor(); Floors[32].setCol(7); // Bony Ground
+            Floors[33] = new cFloor(ref Floors[0]); // Crops
+             Floors[33].setRow(7);
+             Floors[33].setCol(7);
+            Floors[34] = new cFloor();
+             Floors[34].setRow(8);
+             Floors[34].setCol(7);
+            //Walkway and grass/walkway
+            Floors[35] = new cFloor(ref Floors[0]); // Walkway
+             Floors[35].setGraphicTemplate(25);
+             Floors[35].setRow(4);
+             Floors[35].setCol(5);
+            Floors[36] = new cFloor(); Floors[36].setCol(6); //Dirty Walkway
+            Floors[37] = new cFloor();//Walkway Edge
+             Floors[37].setRow(4);
+             Floors[37].setCol(0);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
---
+++ G5 Delicious Drayk 3D/MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+ * All floors in G5 are width=82 and height=56 (we forcibly convert them to that size anyway)
+ * An opacity mask will be used to take only the middle portion of each floor cell in a sprite sheet (transparency)
+ * Floors of Zone 1 (Pacification Fields) start with byte 2592 and and each zone (floors+walls+people+etc) consists of 52088 bytes
+ * 
+ * The cells inside each graphics template are referred to as
+ * 00 01 02 03...
+ * 10 11 12 13...
+ * 20 21 22 23...
+ * 30 31 32 33...
+ * ..............
+ * 
+ * DPI http://stackoverflow.com/questions/3745824/loading-image-into-imagesource-incorrect-width-and-height
+ */
+namespace DMCGeneforgeMpTiles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Associate filenames (incl paths) of all the floor sprite sheets with Image containing the entire sprite sheet
+        Dictionary<string, Image> dSpriteSheets = new Dictionary<string, Image>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            #region Grid Lines
+            //Checkbox for whether straight lines are visible
+            System.Windows.Data.Binding binding = new System.Windows.Data.Binding("IsChecked");
+            binding.Mode = System.Windows.Data.BindingMode.TwoWay;
+            binding.Converter = new BooleanToVisibilityConverter(); 
+            binding.Source = cbStraight;
+
+            //Vertical lines
+            for(int i=0;i<1000;i+=82)
+            {
+                Line lNew = new Line();
+                lNew.Stroke = new SolidColorBrush(Colors.Black);
+                lNew.StrokeThickness = 1;
+                lNew.X1 = i; lNew.X2 = i;
+                lNew.Y1 = 0; lNew.Y2 = 500;
+                lNew.SetBinding(UIElement.VisibilityProperty, binding);
+                cOuter.Children.Add(lNew);
+            }
+            //Horizontal lines
+            for(int i=0;i<1000;i+=56)
+            {
+                Line lNew = new Line();
+                lNew.Stroke = new SolidColorBrush(Colors.Black);
+                lNew.StrokeThickness = 1;
+                lNew.X1 = 0; lNew.X2 = 1500;
+                lNew.Y1 = i; lNew.Y2 = i;
+                lNew.SetBinding(UIElement.VisibilityProperty, binding);
+                cOuter.Children.Add(lNew);
+            }
+            Binding bIsometric = new Binding("IsChecked");
+            bIsometric.Mode = BindingMode.TwoWay;
+            bIsometric.Converter = new BooleanToVisibilityConverter();
+            bIsometric.Source = cbIsometric;
+            //North to South lines
+            for(int i=0;i<10000;i+=82)
+            {
+                Line lnew = new Line();
+                lnew.Stroke = new SolidColorBrush(Colors.Blue);
+                lnew.StrokeThickness = 1;
+                lnew.X1 = i; lnew.X2 = i-730;
+                lnew.Y1 = 0; lnew.Y2 = 500;
+                lnew.SetBinding(UIElement.VisibilityProperty, bIsometric);
+                cOuter.Children.Add(lnew);
+            }
+            //East to West Lines
+            for (int i = 0; i < 10000; i += 56)
+            {
+                Line lnew = new Line();
+                lnew.Stroke = new SolidColorBrush(Colors.Blue);
+                lnew.StrokeThickness = 1;
+                lnew.X1 = 0; lnew.X2 = 500;
+                lnew.Y1 = i; lnew.Y2 = i+340;
+                lnew.SetBinding(UIElement.VisibilityProperty, bIsometric);
+                cOuter.Children.Add(lnew);
+            }
+#endregion
+
+            Image iG3000=new Image()
+            {
+                Source=new BitmapImage(new Uri(@"a:\g3000.bmp"))
+            };
+            dSpriteSheets.Add("g3000.bmp", iG3000);
+            System.Drawing.Bitmap bmp = Paloma.TargaImage.LoadTargaImage(@"a:\g1000.tga");
+            bmp.Save(@"a:\G1000.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
+            Image iG1000 = new Image()
+            {
+                Source = new BitmapImage(new Uri(@"a:\g1000.bmp"))
+            };
+            dSpriteSheets.Add("g1000.tga",iG1000);
+            bmp = Paloma.TargaImage.LoadTargaImage(@"a:\g1550.tga");
+            bmp.Save(@"a:\G1550.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
+            Image iG1550 = new Image()
+            {
+                Source = new BitmapImage(new Uri(@"a:\g1550.bmp"))
+            };
+            dSpriteSheets.Add("g1550.tga", iG1550);
+
+            cFloor.InitializeFloors();
+            LoadZone(1);
+            
+        }
+
+        /// <summary>
+        /// Reads from ScenDat -> go to a zone and load the floors and walls onto cOuter 
+        /// </summary>
+        /// <param name="num"></param>
+        public void LoadZone(int num)
+        {
+            System.IO.FileStream fsScenData = new System.IO.FileStream(@"a:\agf5ScenData.dat",System.IO.FileMode.Open);
+            fsScenData.Seek(2592,System.IO.SeekOrigin.Begin); // Pacification Fields
+            int iFloorByte;
+            int iCell=0;
+            double dStartX=2500, dStartY=0; // where the West edge starts drawing on the canvas
+           
+            double i = dStartX;
+            double j = dStartY;
+            while(iCell<4096)
+            {
+                iFloorByte = (short)fsScenData.ReadByte();
+                
+                //usByte2 = System.Net.IPAddress.NetworkToHostOrder(usByte2);
+                //get which floor this int represents
+                //create image with source defined floor and place on screen
+                Image imgCell = new Image();
+                //if(usByte2<50)
+                CroppedBitmap cbReturnedBitmap = GetFloor(iFloorByte);
+                if (cbReturnedBitmap == null)
+                { 
+                    imgCell.Visibility = Visibility.Collapsed;
+                    i -= 41;
+                    j += 28;
+                    iCell++;
+                    continue; 
+                }
+                imgCell.Tag = (string)iCell.ToString() +" ; "+ i +" ; "+ j;
+                imgCell.MouseDown+=iCell2_MouseDown;
+                imgCell.Source = cbReturnedBitmap;
+                imgCell.Width = 82;
+                imgCell.Height = 56;
+                MakeTransparent(ref imgCell);
+                imgCell.SetValue(Canvas.LeftProperty, i);
+                imgCell.SetValue(Canvas.TopProperty,  j);
+                cOuter.Children.Add(imgCell);
+                i -= 41;
+                j += 28;
+               iCell++;
+
+                if(iCell%64==0) // if is the beginning of a new column
+                {
+                    int iWhichCol = iCell / 64;
+                    i = dStartX + 41 * iWhichCol;
+                    j = dStartY + 28 * iWhichCol;
+                }
+            }
+            //usByte2 = brScenData.ReadUInt16();
+            //usByte2 = brScenData.ReadUInt16();
+            //MessageBox.Show(usByte2.ToString());
+            //brScenData.Dispose();
+            fsScenData.Dispose();
+        }
+        /// <summary>
+        /// Calls into cFloor and retrieves floor sprite row and col
+        /// </summary>
+        /// <param name="iFloorNum"></param>
+        /// <returns></returns>
+        CroppedBitmap GetFloor(int iFloorNum)
+        {
+            CroppedBitmap cbFloor=null;
+            //let's assume the snow floorset for now
+            //in all sprite sheets, graphics are rowwise
+            //in G1000.tga (Grass), 8 cells in a row and 10 cells in a column
+            //in G1550.tga (Snow) , same as above
+            int iRow = -1;
+            int iCol = -1;
+
+            if (cFloor.RetrieveRowAndCol(iFloorNum, ref iRow, ref iCol) == false)
+                return cbFloor;
+            cbFloor = GetCell("g1550.tga",iRow,iCol );
+            return cbFloor;
+        }
+
+        /// <summary>
+        /// Get one cell from sprite sheet
+        /// </summary>
+        /// <param name="sSpriteSheet">Filename of sprite sheet</param>
+        /// <returns>Cropped image as a new CroppedBitmap</returns>
+        public CroppedBitmap GetCell(string sSpriteSheet,int iStartX,int iStartY,int iEndX,int iEndY)
+        {
+            Image iWhichSpriteSheet;
+            dSpriteSheets.TryGetValue(sSpriteSheet, out iWhichSpriteSheet);
+            
+            CroppedBitmap cbNew = new CroppedBitmap();
+            cbNew.BeginInit();
+            cbNew.Source = (BitmapSource)iWhichSpriteSheet.Source;
+            cbNew.SourceRect = new Int32Rect(iStartX,iStartY,iEndX,iEndY);
+            cbNew.EndInit();
+
+            return cbNew;
+        }
+
+        public CroppedBitmap GetCell(string sSpriteSheet,int iRow,int iCol)
+        {
+            //iRow and iCol begin at 1
+            //The first cell begins at 1px,1px and there's a separation of 1 px between all cells
+
+            CroppedBitmap cbNew = GetCell(sSpriteSheet, iCol * 82+iCol, iRow * 56+iRow, 82,56);
+            return cbNew;
+        }
+
+        /// <summary>
+        /// Make a raw floor into a nice displayable floor without white background
+        /// </summary>
+        /// <param name="iRaw"></param>
+        /// <returns></returns>
+        public void MakeTransparent(ref Image iRaw)
+        {
+            iRaw.OpacityMask = new ImageBrush(new BitmapImage(new Uri(@"a:\g5opacitymask.png")));
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Image iCell2 = new Image();
+            iCell2.Source=GetCell("g3000.bmp",0,0,82,56);
+            MakeTransparent(ref iCell2);
+
+            
+
+            iCell2.SetValue(Canvas.LeftProperty, (double)41);
+            iCell2.SetValue(Canvas.TopProperty , (double)28);
+            
+            cOuter.Children.Add(iCell2);
+        }
+
+        private void cOuter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Random rRandom = new Random();
+
+            Point pMouse = Mouse.GetPosition(Application.Current.MainWindow);
+            Image iCell2 = new Image();
+            iCell2.Source = GetCell("g1000.tga",rRandom.Next(0,5),rRandom.Next(0,5));
+            MakeTransparent(ref iCell2);
+            iCell2.Width = 82;
+            iCell2.Height = 56;
+            iCell2.MouseDown += iCell2_MouseDown;
+
+            double dXPos = Math.Floor((pMouse.X / 41))*41;
+            double dYPos = Math.Floor((pMouse.Y / 28))*28;
+
+            iCell2.SetValue(Canvas.LeftProperty, dXPos);
+            iCell2.SetValue(Canvas.TopProperty, dYPos);
+
+            cOuter.Children.Add(iCell2);
+        }
+
+        void iCell2_MouseDown(object sendero, MouseButtonEventArgs e)
+        {
+            Image sender = (Image)sendero;
+            MessageBox.Show((string)sender.Tag);
+        }
+    }
+}
